82e5a188f420949c2a6ea819508a3b950696ab4a
auvargas <aaron_uziel@live.com>
2026-06-28 18:23:21 -0400

    Paso rojo:
    argumentos de libros ilegales (null) isbn y titulo

--- src/test/java/BibliotecaTest.java
+++ src/test/java/BibliotecaTest.java
@@ -13,12 +13,34 @@ public class BibliotecaTest {
             1605
         );
 
-        // assertTrue(true);
-
         assertEquals("978-3-16-148410-0", libro.getIsbn());
         assertEquals("El Quijote", libro.getTitulo());
         assertEquals("Miguel de Cervantes", libro.getAutor());
         assertEquals(1605, libro.getAnioPublicacion());
         assertTrue(libro.getDisponible());
     }
+
+    @Test
+    void testCrearLibroInvalido(){
+        IllegalArgumentException ex_isbn_null = assertThrows(
+            IllegalArgumentException.class,
+            () -> new Libro( 
+                null, 
+                "hola", 
+                "aaron", 
+                3000
+            )
+        );
+        assertEquals("'isbn' argument cannot be null", ex_isbn_null.getMessage());
+
+        IllegalArgumentException ex_titulo_null = assertThrows(
+            IllegalArgumentException.class,
+            () -> new Libro(
+                "123123-123-123-12-3",
+                null,
+                "aaron",
+                1231)
+        );
+        assertEquals("'titulo' argument cannot be null", ex_titulo_null.getMessage());
+    };
 }
