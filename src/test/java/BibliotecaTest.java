b5bb6096c252a1528e2d01d29f6260ef8cc2984a
auvargas <aaron_uziel@live.com>
2026-06-28 17:28:53 -0400

    fase roja

--- src/test/java/BibliotecaTest.java
+++ src/test/java/BibliotecaTest.java
@@ -4,8 +4,20 @@ import static org.junit.jupiter.api.Assertions.*;
 public class BibliotecaTest {
 
     @Test
-    void testPrimerCasoTDD() {
+    void testCrearLibroValido() {
         // prueba Red?
-        assertTrue(true);
+        Libro libro = new Libro(
+            "978-3-16-148410-0",
+            "El Quijote",
+            "Miguel de Cervantes",
+            1605
+        );
+
+        // assertTrue(true);
+
+        assertEquals("978-3-16-148410-0", libro.getIsbn());
+        assertEquals("El Quijote", libro.getTitulo());
+        assertEquals("Miguel de Cervantes", libro.getAutor());
+        assertEquals(1605, libro.getAnioPublicacion());
     }
 }
