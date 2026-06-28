e1be1602622b90ef0d4538be6b6a9414d6c41c14
auvargas <aaron_uziel@live.com>
2026-06-28 18:42:15 -0400

    Libro | Test | Rename
    Se renombra el archivo porque me di cuenta que estaba trabajando para el archivo de la biblioteca

--- src/test/java/LibroTest.java
+++ src/test/java/LibroTest.java
@@ -1,7 +1,7 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class BibliotecaTest {
+public class LibroTest {
 
     @Test
     void testCrearLibroValido() {
