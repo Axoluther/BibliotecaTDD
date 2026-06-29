724c80eb10f5104e6cbd53d176e63cb2ee15a6cb
auvargas <aaron_uziel@live.com>
2026-06-28 21:24:33 -0400

    Biblioteca | Duplicado | Tests
    Fase verde. Se detecta duplicado, se verá si se puede refactorizar

--- src/main/java/Biblioteca.java
+++ src/main/java/Biblioteca.java
@@ -8,7 +8,13 @@ public class Biblioteca {
     private final Map<String, Libro> libros = new HashMap<>();
 
     public void registrarLibro(Libro libro) {
-        libros.put(libro.getIsbn(), Objects.requireNonNull(libro)) ;
+        Libro libro_nn = Objects.requireNonNull(libro) ;
+        String isbn = libro_nn.getIsbn();
+        Optional<Libro> res = Optional.ofNullable(libros.get(isbn));
+        if ( res.isPresent() ){
+            throw new LibroDuplicadoException(isbn);
+        };
+        libros.put(libro.getIsbn(), libro_nn) ;
     }
 
     public Optional<Libro> buscarPorIsbn(String isbn) {
